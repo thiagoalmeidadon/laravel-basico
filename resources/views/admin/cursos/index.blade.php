257dc82d0687432f3222b47b10d6fd95f5a28a6a
thiagoalmeidadon <thiagodons@gmail.com>
2020-02-18 20:10:06 -0300

    Layout com Materialize e lista de registros

---
+++ resources/views/admin/cursos/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layout.site')
+
+@section('titulo','Cursos')
+
+
+@section('conteudo')
+<div class="container">
+    <h3 class="center">Lista de cursos</h3>
+    <div class="row">
+        <table>
+            <thead>
+                <tr>
+                    <th> ID  </th>
+                    <th> Titulo  </th>
+                    <th> Descrição  </th>
+                    <th> imagem  </th>
+                    <th> Publicado  </th>
+                    <th> Ação  </th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach($registros as $registro)
+                <tr>
+                    <td> {{ $registro->id }} </td>
+                    <td> {{ $registro->titulo }} </td>
+                    <td> {{ $registro->descricao }} </td>
+                    <td> <img width="120" src="{{ asset($registro->imagem) }}" alt="{{ $registro->titulo }}" />  </td>
+                    <td> {{ $registro->publicado }} </td>
+                    <td>
+                        <!-- Enviando o id em conjunto para poder editar -->
+                        <a href="{{ route('admin.cursos.editar', $registro->id ) }}" class="btn deep-orange">Editar</a>
+                        <a href="{{ route('admin.cursos.deletar', $registro->id ) }}" class="btn red">Deletar</a>
+                    </td>
+                </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+    <div class="row">
+        <a href="{{ route('admin.cursos.adicionar') }}" class="btn green center">Adicionar</a>
+    </div>
+
+</div>
+@endsection
--- resources/views/layout/_includes/rodape.blade.php
+++ resources/views/layout/_includes/rodape.blade.php
@@ -1,2 +1,13 @@
+<!--JavaScript at end of body for optimized loading-->
+<!--<script type="text/javascript" src="js/materialize.min.js"></script>-->
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        M.updateTextFields();
+         $('.sidenav').sidenav();
+    });
+</script>
 </body>
 </html>
